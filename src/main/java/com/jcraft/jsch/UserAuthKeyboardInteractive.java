9abba6164e2fb7a6ff86491bb9a2fedc44589cc7
Jeremy Norris <jeremy.norris@lbnetworks.co>
2026-08-27 06:06:50 -0500

    #1139: add bounds checks when parsing prompts from SSH_MSG_USERAUTH_INFO_REQUEST messages.

--- src/main/java/com/jcraft/jsch/UserAuthKeyboardInteractive.java
+++ src/main/java/com/jcraft/jsch/UserAuthKeyboardInteractive.java
@@ -121,6 +121,10 @@ class UserAuthKeyboardInteractive extends UserAuth {
           String instruction = Util.byte2str(buf.getString());
           String languate_tag = Util.byte2str(buf.getString());
           int num = buf.getInt();
+          // Min length of one string plus one byte is 5 bytes.
+          if (num < 0 || num * 5 > buf.getLength()) {
+            throw new JSchException("invalid prompt count");
+          }
           String[] prompt = new String[num];
           boolean[] echo = new boolean[num];
           for (int i = 0; i < num; i++) {
